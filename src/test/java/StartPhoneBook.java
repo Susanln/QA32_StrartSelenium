5852151b7bd5b34943cd0ae978d10f52b1d2fb7d
Susanln <susanln@mail.ru>
2022-03-10 11:47:35 +0200

    add Table

--- src/test/java/StartPhoneBook.java
+++ src/test/java/StartPhoneBook.java
@@ -50,15 +50,28 @@ public class StartPhoneBook {
     @Test
     public void cssLocatorsTest(){
         wd = new ChromeDriver();
-        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app");
+        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/login");
         wd.manage().window().maximize();
 
         wd.findElement(By.cssSelector("div"));
+
+             wd.findElement(By.xpath("//div"));
+
        // wd.findElement(By.cssSelector(".login_login__3EHKB"));
         wd.findElement(By.cssSelector("#root"));
-        //wd.findElement(By.cssSelector("[placeholder='Password']"));
+             wd.findElement(By.xpath("//a")); //    /html/body/div/a
+
+             wd.findElement(By.xpath("//*[@class='login_login__3EHKB']"));
+             wd.findElement(By.xpath("//*[@id='root']"));
+
+             wd.findElement(By.xpath("//*[@placeholder='Email']"));
+             wd.findElement(By.xpath("//*[@placeholder = 'Password']"));
+             //wd.findElement(By.cssSelector("[placeholder='Password']"));
+
         wd.findElement(By.cssSelector("[placeholder*='ss']"));
+        wd.findElement(By.xpath("//*[contains(@placeholder,'Password')]"));
         wd.findElement(By.cssSelector("[placeholder^='Pa']"));
+        wd.findElement(By.xpath("//*[starts-with(@placeholder,'Pa')]"));
         wd.findElement(By.cssSelector("[placeholder$='d']"));
 
 
--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TableTest {
@@ -14,6 +15,88 @@ public class TableTest {
         wd.manage().window().maximize();
 
 
+    }
+    @Test
+    public void xPathTests()
+    {
+        wd = new ChromeDriver();
+        wd.navigate().to("file:///C:/Users/susan/Downloads/index.html");
+        wd.manage().window().maximize();
+
+        WebElement item1 = wd.findElement(By.xpath("//a[@href='#item1']"));
+        String textItem1 = item1.getText();
+        //System.out.println("My text   " + textItem1);
+        // Assert.assertEquals(textItem1,"Item 1");
+        item1.click();
+        WebElement check = wd.findElement(By.xpath("//*[@class='message']"));
+        String textCheck = check.getText();
+        Assert.assertEquals(textCheck,"Clicked by Item 1");
+
+        WebElement item2 = wd.findElement(By.xpath("//a[@href='#item2']"));
+        item2.click();
+        check = wd.findElement(By.xpath("//*[@class='message']"));
+        textCheck = check.getText();
+        Assert.assertEquals(textCheck,"Clicked by Item 2");
+
+        WebElement item3 = wd.findElement(By.xpath("//a[@href='#item3']"));
+        item3.click();
+        check = wd.findElement(By.xpath("//*[@class='message']"));
+        textCheck = check.getText();
+        Assert.assertEquals(textCheck,"Clicked by Item 3");
+
+        WebElement item4 = wd.findElement(By.xpath("//a[@href='#item4']"));
+        item4.click();
+        check = wd.findElement(By.xpath("//*[@class='message']"));
+        textCheck = check.getText();
+        Assert.assertEquals(textCheck,"Clicked by Item 4");
+
+        WebElement europe = wd.findElement(By.xpath("//*[text()='Europe']"));
+        String textEurope = europe.getText();
+        Assert.assertEquals(textEurope,"Europe");
+
+        WebElement asia = wd.findElement(By.xpath("//*[text()='Asia']"));
+        String textAsia = asia.getText();
+        Assert.assertEquals(textAsia,"Asia");
+
+        WebElement america = wd.findElement(By.xpath("//*[text()='America']"));
+        String textAmerica = america.getText();
+        Assert.assertEquals(textAmerica,"America");
+
+        WebElement ukraine = wd.findElement(By.xpath("//*[text()='Ukraine']"));
+        String textUkraine = ukraine.getText();
+        Assert.assertEquals(textUkraine,"Ukraine");
+
+        WebElement israel = wd.findElement(By.xpath("//*[text()='Israel']"));
+        String textIsrael = israel.getText();
+        Assert.assertEquals(textIsrael,"Israel");
+
+        WebElement usa = wd.findElement(By.xpath("//*[text()='USA']"));
+        String textUsa = usa.getText();
+        Assert.assertEquals(textUsa,"USA");
+
+        WebElement germany = wd.findElement(By.xpath("//*[text()='Germany']"));
+        String textGermany = germany.getText();
+        Assert.assertEquals(textGermany,"Germany");
+
+        WebElement russia = wd.findElement(By.xpath("//*[text()='Russia']"));
+        String textRussia = russia.getText();
+        Assert.assertEquals(textRussia,"Russia");
+
+        WebElement canada= wd.findElement(By.xpath("//*[text()='Canada']"));
+        String textCanada = canada.getText();
+        Assert.assertEquals(textCanada,"Canada");
+
+        WebElement poland= wd.findElement(By.xpath("//*[text()='Poland']"));
+        String textPoland = poland.getText();
+        Assert.assertEquals(textPoland,"Poland");
+
+        WebElement chine= wd.findElement(By.xpath("//*[text()='Chine']"));
+        String textChine = chine.getText();
+        Assert.assertEquals(textChine,"Chine");
+
+        WebElement mexico= wd.findElement(By.xpath("//*[text()='Mexico']"));
+        String textMexico = mexico.getText();
+        Assert.assertEquals(textMexico,"Mexico");
     }
     @Test
 
@@ -47,6 +130,11 @@ public class TableTest {
         WebElement Mexico = wd.findElement(By.cssSelector("#country-table tr:last-child td:last-child"));
 
         WebElement allert = wd.findElement(By.cssSelector("#alert"));
+        wd.findElement(By.xpath("//*[text()='Maria Anders']"));
+        wd.findElement((By.xpath("//*[@id='customers']//tr[2]/td[2]")));
+        wd.findElement(By.xpath("//input[1]/ancestor::div[last()]")); //  ищет родителя
+        wd.findElement(By.xpath("//input[1]/following-sibling::button[2]")); // ищет в пределах этого уровня вниз
+        wd.findElement(By.xpath("//button[2]/preceding-sibling::"));// ищет в пределах этого уровня вверх
 
     }
 }
